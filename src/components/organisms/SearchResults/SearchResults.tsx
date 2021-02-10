7e7dac08455affa245af85b2d3b999380448f0d6
Janusz Guzowski <janusz.guzowski@gmail.com>
2021-02-11 00:11:20 +0100

    Improve result component

--- src/components/organisms/SearchResults/SearchResults.tsx
+++ src/components/organisms/SearchResults/SearchResults.tsx
@@ -1,7 +1,11 @@
 import * as React from "react";
 import styles from "./SearchResults.module.scss";
 import { Container } from "@material-ui/core";
-
+import {
+  ThumbUpAlt,
+  FavoriteOutlined,
+} from "@material-ui/icons";
+import VisibilityIcon from "@material-ui/icons/Visibility";
 import picture from "../../../assets/spaghetti-big.webp";
 import { Link } from "react-router-dom";
 import IRecipe from "../../../models/IRecipe";
@@ -33,7 +37,30 @@ const searchResults = (props: any) => {
               </h2>
               <div className={styles.shortLine}> </div>
               <p className={styles.time}>{r.prepareTime} minut przygotowania</p>
-              <p>{r.description}</p>
+              <Ingredients>
+                {r.recipeLines.map((line) => (
+                  <Ingredient>
+                    <div>{line.ingredient.name}</div>
+                    <Small>
+                      {line.amount} {line.unit.name}
+                    </Small>
+                  </Ingredient>
+                ))}
+              </Ingredients>
+              <Icons>
+                <Icon>
+                  <FavoriteOutlined color={"secondary"} />
+                  <div>{r.inFavourite}</div>
+                </Icon>
+                <Icon>
+                  <ThumbUpAlt color={"action"} />
+                  <div>{r.likes}</div>
+                </Icon>
+                <Icon>
+                  <VisibilityIcon />
+                  <div>{r.viewCounter}</div>
+                </Icon>
+              </Icons>
             </div>
           </div>
         ))
@@ -52,4 +79,34 @@ const NoResult = styled.div`
   text-align: center;
   margin-top: 20px;
   color: red;
+`;
+
+const Ingredients = styled.div`
+  display: flex;
+`;
+
+const Ingredient = styled.div`
+  background-color: white;
+  border: 1px dashed black;
+  border-radius: 15px;
+  padding: 5px 10px;
+  margin: 5px;
+  text-align: center;
+`;
+
+const Small = styled.div`
+  font-weight: bold;
+  font-size: 12px;
+`;
+
+const Icons = styled.div`
+  display: flex;
+  margin-top: 20px;
+`;
+
+const Icon = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 40px;
+  margin: 10px;
 `;
