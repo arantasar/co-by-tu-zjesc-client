eb3ca4a6a941c581a1babd244b7981b04359d0d2
Janusz Guzowski <janusz.guzowski@gmail.com>
2021-04-05 00:25:13 +0200

    Add Docker configuration

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -3,7 +3,7 @@ import axios, { AxiosRequestConfig } from "axios";
 const config: AxiosRequestConfig = {
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "http://server:8080/api"
+      ? "http://cobytuzjesc.pl:5000/api"
       : "http://localhost:5000/api",
 };
 const instance = axios.create(config);
